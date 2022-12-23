67905cf889bbdb5b9edbd2562c6497d1b2e613d6
GArifullinII <ildaridrisovich@gmail.com>
2022-12-23 18:52:48 +0400

    feat: added DisplayNoteViewController

---
+++ NotesCFT/Controllers/DisplayNoteViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DisplayNoteViewController.swift
+//  NotesCFT
+//
+//  Created by Ildar Garifullin on 23/12/2022.
+//
+
+import UIKit
+
+class DisplayNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
