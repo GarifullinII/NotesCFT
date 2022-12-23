1c2d97b7e42dec644681e4d5d74c5010f85214c3
GArifullinII <ildaridrisovich@gmail.com>
2022-12-23 17:48:23 +0400

    feat: added custom table view cell

--- NotesCFT/Controllers/ListNotesTableViewController.swift
+++ NotesCFT/Controllers/ListNotesTableViewController.swift
@@ -25,9 +25,10 @@ class ListNotesTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath)
-        cell.textLabel?.text = "Cell Row: \(indexPath.row) Section: \(indexPath.section)"
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
+        cell.noteTitleLabel.text = "note's title"
+        cell.noteModificationTimeLabel.text = "note's modification time"
+        
         return cell
     }
 
