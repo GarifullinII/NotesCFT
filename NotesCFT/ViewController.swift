9b93507434e354c9b0db97889dce5e8b8a0a041f
GArifullinII <ildaridrisovich@gmail.com>
2022-12-23 14:40:53 +0400

    Initial Commit

---
+++ NotesCFT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesCFT
+//
+//  Created by Ildar Garifullin on 23/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
